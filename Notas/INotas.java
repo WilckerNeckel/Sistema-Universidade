418ae4226dca564bcf8842a617b294fcb63d5ae2
Wilcker Neckel <wilckerwrsn@gmail.com>
2024-12-02 11:22:53 -0400

    feat: implementa interface para notas

---
+++ Notas/INotas.java
@@ -0,0 +1,11 @@
+package trabalhofinal.sistemauniversidade.Notas;
+import trabalhofinal.sistemauniversidade.pessoas.Aluno;
+
+public interface INotas {
+
+    public double mediaGeral(Aluno aluno);
+    public Nota notaAluno1Un(Aluno aluno);
+    public Nota notaAluno2Un(Aluno aluno);
+    public Nota notaAluno3Un(Aluno aluno);
+
+}
